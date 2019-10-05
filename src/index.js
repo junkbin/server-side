153b97feccf9fdea9c0f6bdf59eee2e849df8a2e
santosh <santosh.ece06@gmail.com>
2019-10-05 11:13:01 +0530

    indes.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,21 @@ let app = express();
 app.use(cors());
 
 
+/** GET RETURNS ALL PRODUCT */
 app.get("/", function(req, res) {
     let output = {productId: 1, productName : 'VIVO',  categoryName: 'Mobile', categoryId: 'V100'};
     res.json(output);
 });
 
+
+/** POST SAVE PRODUCT INTO DB */
 app.post("/", function(req, res){
     let output = {productId: 1, productName : 'VIVO',  categoryName: 'Mobile', categoryId: 'V100'};
     res.json(output);
 });
 
 
+/** STARTHE THE SERVER */
 app.listen(3000, function() {
     console.log('server.started');
 });
