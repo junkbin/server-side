ff55c4347ddcdfea7c4c11aa2ef3222a0788d4f9
santosh <santosh.ece06@gmail.com>
2019-10-05 11:12:24 +0530

    get.post.method.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 let express = require('express');
+let cors = require('cors')
 
 let app = express();
+
+// for cross origin request.
+app.use(cors());
+
+
+app.get("/", function(req, res) {
+    let output = {productId: 1, productName : 'VIVO',  categoryName: 'Mobile', categoryId: 'V100'};
+    res.json(output);
+});
+
+app.post("/", function(req, res){
+    let output = {productId: 1, productName : 'VIVO',  categoryName: 'Mobile', categoryId: 'V100'};
+    res.json(output);
+});
 
 
 app.listen(3000, function() {
